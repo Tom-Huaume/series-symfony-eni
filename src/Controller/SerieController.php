7e9703c4560ac4501e92bcfd7cb08d97f46506b3
Tom-Huaume <thuaume1@gmail.com>
2021-08-01 17:29:29 +0200

    Ajout fonctions supprimer / modifier du entityManager

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/series", name="serie_")
  */
-class SerieControllers extends AbstractController
+class SerieController extends AbstractController
 {
     /**
      * @Route("", name="list")
@@ -43,9 +45,49 @@ class SerieControllers extends AbstractController
 
     /**
      * @Route("/demo", name="em-demo")
+     * @param EntityManagerInterface $entityManager
+     * @return Response
      */
-    public function demo(): Response
+    public function demo(EntityManagerInterface $entityManager): Response
     {
+        // Façon alternative de récupérer l'entityManager :
+        //$entityManager = $this->getDoctrine()->getManager();
+
+        //créé une instance
+        $serie = new Serie();
+
+        //hydrate toutes les propriétés
+        $serie->setName('Altered Carbon');
+        $serie->setBackdrop('altered-carbon-68421.jpg');
+        $serie->setPoster('altered-carbon-68421.jpg');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("- 1 year"));
+        $serie->setLastAirDate(new \DateTime("- 6 month"));
+        $serie->setGenres('Sci-Fi');
+        $serie->setOverview('Altered Carbon is set in the 23rd 
+        century when the human mind has been digitized and the soul it 
+        self is transferable from one body to the next. Takeshi Kovacs, 
+        a former elite interstellar warrior known as an Envoy who has been 
+        imprisoned for 250 years, is downloaded into a future he\'d tried to stop.');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus('returning');
+        $serie->setTmdbId(68421);
+
+        dump($serie);
+
+        //ENREGISTRER UNE SERIE
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        //MODIFIER UNE SERIE
+        //$serie->setGenres('comedy');
+        //$entityManager->flush();
+
+        //SUPPRIMER UNE SERIE
+        //$entityManager->remove($serie);
+        //$entityManager->flush();
+
         return $this->render('serie/create.html.twig');
     }
 }
